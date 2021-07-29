907b24d02dfd87dd5baebc58f205fbdd22874c37
qkrrbgy <pkh13270@gmail.com>
2021-07-29 15:14:08 +0900

    Spring Boot With React Example

--- src/components/input.js
+++ src/components/input.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Input(props) {
     return (
-        <form onSubmit={props.handleSubmit}>
+        <form onSubmit={props.handleSubmit }>
             <label>
                 Todo &nbsp;
                 <input type="text" required={true} value={props.input} onChange={props.handleChange} />
