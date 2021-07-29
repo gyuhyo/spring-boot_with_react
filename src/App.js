c48e85249568c8f2fe13102db1d57b0946c0a75a
qkrrbgy <pkh13270@gmail.com>
2021-07-29 15:12:12 +0900

    Spring Boot With React Example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
 import logo from './logo.svg';
+import React,{ useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
+import Input from "./components/input"
+import Todo from "./components/todo"
 
 function App() {
-  return (
+    const baseUrl = "http://localhost:8081";
+
+    const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState("");
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    async function getTodos() {
+        await axios
+            .get(baseUrl + "/todo")
+            .then((response) => {
+                setTodos(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    function register(e) {
+        e.preventDefault();
+
+        const insertTodo = async () => {
+            await axios
+                .post(baseUrl + "/todo", {
+                    todoName: input
+                })
+                .then((response) => {
+                    console.log(response);
+                    setInput("");
+                    getTodos();
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+        insertTodo();
+        console.log("할 일이 추가됨.");
+    }
+
+    function modify(id) {
+        console.log(id);
+        const modifyTodo = async () => {
+            await axios
+                .put(baseUrl + "/todo/" + id, {})
+                .then((response) => {
+                    setTodos(
+                        todos.map((todo) =>
+                            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+                        )
+                    )
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+        modifyTodo();
+        console.log("할 일이 수정됨.");
+    }
+
+    function remove(id) {
+        console.log(id);
+        const removeTodo = async () => {
+            await axios
+                .delete(baseUrl + "/todo/" + id, {})
+                .then((response) => {
+                    setTodos(
+                       todos.filter((todo) => todo.id !== id)
+                    )
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+        removeTodo();
+        console.log("할 일이 추가됨.");
+    }
+
+    function changeText(e) {
+        e.preventDefault();
+        setInput(e.target.value);
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>TODO LIST</h1>
+        <Input handleSubmit={register} input={input} handleChange={changeText} />
+
+        {
+            todos ? todos.map((todo) => {
+                return (
+                    <Todo key={todo.id} todo={todo} handleClick={() => modify(todo.id)} handleDelete={() => remove(todo.id)} />
+                )
+            }) : null
+        }
+
     </div>
-  );
+    );
 }
 
 export default App;
